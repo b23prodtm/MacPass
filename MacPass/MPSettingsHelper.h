ec37c22fd095197419cccc9d5017c434a3f2ec13
Tina B Rakoto <webmaster@b23prodtm.info>
2025-03-08 10:27:25 +0100

    XIB builder failed

--- MacPass/MPSettingsHelper.h
+++ MacPass/MPSettingsHelper.h
@@ -132,9 +132,15 @@ typedef NS_ENUM(NSUInteger, MPFaviconDownloadMethod) {
 // the values are mapped to NSControlStateValue for backwards compatibilty
 // older implementations did use a checkbox to store this setting
 typedef NS_ENUM(NSInteger, MPTouchIDKeyStorage) {
+#if __MAC_OS_X_VERSION_MAX_ALLOWED <= 101300
+  MPTouchIDKeyStorageTransient = NSMixedState,
+  MPTouchIDKeyStorageDisabled = NSOffState,
+  MPTouchIDKeyStoragePersistent = NSOnState
+#else
   MPTouchIDKeyStorageTransient = NSControlStateValueMixed,
   MPTouchIDKeyStorageDisabled = NSControlStateValueOff,
   MPTouchIDKeyStoragePersistent = NSControlStateValueOn
+#endif
 };
 
 /* Password Generation */
